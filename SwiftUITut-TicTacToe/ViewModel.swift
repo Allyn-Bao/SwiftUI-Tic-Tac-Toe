c91bb3f57c66f15c0d48af86e17806627037b1ba
Allyn Bao <baoallyn@gmail.com>
2022-01-17 10:04:37 -0500

    improved computer algorithm to add challenge to the game

--- SwiftUITut-TicTacToe/ViewModel.swift
+++ SwiftUITut-TicTacToe/ViewModel.swift
@@ -12,6 +12,11 @@ final class ViewModel : ObservableObject {
                                GridItem(.flexible()),
                                GridItem(.flexible()),]
     
+    let winPatterns: Set<Set<Int>> = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]]
+    
     @Published var isHumansTurn = true
     @Published var moves : [Move?] = Array(repeating: nil, count: 9)
     @Published var gameEnds : Bool = false
@@ -44,15 +49,45 @@ final class ViewModel : ObservableObject {
     }
     
     func determineComputerMoveBoardIndex() -> Int {
-        var movePosition = Int.random(in: 0..<9)
-        while !isAvaliable(boardIndex: movePosition) {
-            movePosition = Int.random(in: 0..<9)
+        if isAvaliable(boardIndex: 4) {
+            return 4 // take cnter if possiable
+        } else {
+            // to block
+            let humanToWinIndex = getMoveIndexToWin(for: .human)
+            if humanToWinIndex != -1 {
+                return humanToWinIndex
+            }
+            // to win
+            let computerToWinIndex = getMoveIndexToWin(for: .computer)
+            if computerToWinIndex != -1 {
+                return computerToWinIndex
+            }
+            // random
+            var randomIndex = Int.random(in: 0..<9)
+            while !self.isAvaliable(boardIndex: randomIndex) {
+                randomIndex = Int.random(in: 0..<9)
+            }
+            return randomIndex
         }
-        return movePosition
     }
     
     func isAvaliable(boardIndex: Int) -> Bool {
         return moves[boardIndex] == nil
+    }
+    
+    func getMoveIndexToWin(for player: Player) -> Int {
+        let playerPositions = getPositionSet(for: player)
+        for pattern in self.winPatterns where playerPositions.isSubset(of: pattern) {
+            let possiableMove = pattern.subtracting(playerPositions)
+            if !possiableMove.isEmpty {
+                for index in possiableMove {
+                    if self.isAvaliable(boardIndex: index) {
+                        return index
+                    }
+                }
+            }
+        }
+        return -1
     }
     
     func allPositionsTaken() -> Bool {
@@ -60,17 +95,19 @@ final class ViewModel : ObservableObject {
     }
     
     func checkWin(for player: Player) -> Bool {
-        let winPatterns: Set<Set<Int>> = [
-            [0, 1, 2], [3, 4, 5], [6, 7, 8],
-            [0, 3, 6], [1, 4, 7], [2, 5, 8],
-            [0, 4, 8], [2, 4, 6]]
+        
         // compact Map : remove nil's
         // filter {} keeps this player's move only
+        
+        let playerPositions = self.getPositionSet(for: player)
+        for pattern in self.winPatterns where pattern.isSubset(of:playerPositions) { return true }
+        return false
+    }
+    
+    func getPositionSet(for player: Player) -> Set<Int> {
         let playerMoves = moves.compactMap { $0 }.filter { $0.player == player }
         let playerPositions = Set(playerMoves.map { $0.boardIndex })
-        
-        for pattern in winPatterns where pattern.isSubset(of:playerPositions) { return true }
-        return false
+        return playerPositions
     }
     
     func ckeckGameResult() {
